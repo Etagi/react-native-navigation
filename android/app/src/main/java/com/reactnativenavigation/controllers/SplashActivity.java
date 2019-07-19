8f0199b1fdef02916cf524fe00d8c5f9ac352c7f
Khisamov <53078574+Khisamov@users.noreply.github.com>
2019-07-19 14:41:58 +0500

    Update SplashActivity.java
    request permission before starting JS

--- android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
@@ -9,12 +9,27 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.*;
 import com.reactnativenavigation.utils.CompatUtils;
 
 public abstract class SplashActivity extends AppCompatActivity {
     public static boolean isResumed = false;
+
+    public static int PERMISSION_REQUEST_CODE = 10001;
+
+    private static final String MY_SETTINGS = "my_settings";
+    SharedPreferences mSettings;
 
     public static void start(Activity activity) {
         Intent intent = activity.getPackageManager().getLaunchIntentForPackage(activity.getPackageName());
@@ -30,9 +45,23 @@ public abstract class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mSettings = getSharedPreferences(MY_SETTINGS, Context.MODE_PRIVATE);
+
         LaunchArgs.instance.set(getIntent());
         setSplashLayout();
         IntentDataHandler.saveIntentData(getIntent());
+    }
+
+    public static boolean shouldAskPermission() {
+        return Build.VERSION.SDK_INT >= 23 && !Settings.canDrawOverlays(NavigationApplication.instance);
+    }
+
+    @TargetApi(23)
+    public static void askOverlayPermission(Context context, String packageName) {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + packageName));
+        context.startActivity(intent);
     }
 
     @Override
@@ -57,6 +86,47 @@ public abstract class SplashActivity extends AppCompatActivity {
             ReactDevPermission.askPermission(this);
             return;
         }
+
+        //----------Permissions-------------------------------------
+        SharedPreferences.Editor editor = mSettings.edit();
+
+        if (Build.VERSION.SDK_INT < 23) {
+            boolean checkReadPhonePerm = mSettings.getBoolean("readPhonePerm", false);
+
+            if (!checkReadPhonePerm && ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_PHONE_STATE}, PERMISSION_REQUEST_CODE);
+
+                editor.putBoolean("readPhonePerm", true);
+                editor.commit();
+                return;
+            }
+
+            boolean checkFineLocationPerm = mSettings.getBoolean("fineLocationPerm", false);
+
+            if (!checkFineLocationPerm && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+                editor.putBoolean("fineLocationPerm", true);
+                editor.commit();
+                return;
+            }
+        }
+
+        boolean checkOverlayPerm = mSettings.getBoolean("hasOverlay", false);
+
+        if (!checkOverlayPerm && shouldAskPermission()) {
+            askOverlayPermission(this, getPackageName());
+
+            editor.putBoolean("hasOverlay", true);
+            editor.commit();
+            return;
+        }
+
+        editor.putBoolean("readPhonePerm", false);
+        editor.putBoolean("fineLocationPerm", false);
+        editor.putBoolean("hasOverlay", false);
+        editor.commit();
+
+        //----------EndPermissions-------------------------------------
 
         if (NavigationApplication.instance.isReactContextInitialized()) {
             NavigationApplication.instance.getEventEmitter().sendAppLaunchedEvent();
